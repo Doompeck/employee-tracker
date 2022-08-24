1b1359df06e00633a26bd5b81fb235fce843a728
Benjamin <bstanf@uw.edu>
2022-08-23 21:40:39 -0700

    added a seed and verified it is populating the tables correctly

--- db/schema.sql
+++ db/schema.sql
@@ -8,33 +8,33 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    d_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (d_id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    r_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
+    PRIMARY KEY (r_id),
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES department(d_id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    e_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id),
+    PRIMARY KEY (e_id),
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE CASCADE
+    REFERENCES role(r_id)
+    ON DELETE CASCADE,
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES employee(e_id)
     ON DELETE CASCADE
 );
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Marketing'),
+('Web Dev'),
+('Executive'),
+('Customer Service');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Rep', 75000, 1),
+('VP of marketing', 80000, 2),
+('Web Developer', 90000, 3),
+('CEO', 1000000, 4),
+('Customer Service Agent', 60000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Larry", "Newhart", 1, 1),
+("Daryl", "Newhart", 2, 1),
+("Daryl", "Notnewhart", 3, 1),
+("Bobby", "Ricky", 2, 1),
+("Carl", "Potts", 4, 5),
+("Arthur", "Dent", 5, 1),
+("Duncan", "Idaho", 3, 1);
